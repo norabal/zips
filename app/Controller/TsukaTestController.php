8996ce80c20885207affd49226416bb882aa6c26
hiroyukitsukano <Hiro@tsukano-ayako-no-MacBook-Pro.local>
2015-09-23 23:39:52 +0900

    add new file

---
+++ app/Controller/TsukaTestController.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+App::uses('AppController', 'Controller');
+
+/**
+ * CakePHP TsukaTestController
+ * @author Hiro
+ */
+class TsukaTestController extends AppController {
+
+    public function index() {
+        echo 'TeraHouse';
+        exit;
+    }
+
+}
